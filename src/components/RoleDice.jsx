c46e4b441f9991905b49376a0134c1097bbafc86
Asmita-Tamang <i.am.asmitatmg1234@gmail.com>
2025-06-19 11:31:34 +0545

    added images

--- src/components/RoleDice.jsx
+++ src/components/RoleDice.jsx
@@ -1,16 +1,24 @@
 import styled from "styled-components";
+import dice1 from "../../public/images/dice/dice_1.png"
+import dice2 from "../../public/images/dice/dice_2.png"
+import dice3 from "../../public/images/dice/dice_3.png"
+import dice4 from "../../public/images/dice/dice_4.png"
+import dice5 from "../../public/images/dice/dice_5.png"
+
+
 
 const RoleDice = ({currentDice, roleDice}) => {
 
   return (
     <DiceContainer>
       <div className="dice" onClick={roleDice}>
-        <img src={"/images/dice_${currentDice}.png"} alt={"dice ${currentDice}"} />
+        <img src={`/dice_game/images/dice/dice_${currentDice}.png`} alt={`dice ${currentDice}`} />
+
       </div>
       <p>Click on Dice to roll</p>
     </DiceContainer>
   );
-};
+}; 
 
 export default RoleDice;
 
